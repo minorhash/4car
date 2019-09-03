7aa30005acb625f86a0ab5e839ed5d0752ed0bff
minorhash <minorhash@gmail.com>
2019-09-03 22:30:14 +0900

    09/03/19

--- rot/lib/cnf/cld.js
+++
@@ -1,5 +0,0 @@
-module.exports={
-  nam: 'g0urmate',
-  key: "966616347112999",
-  sec:"beSKvhoUjma0Sm9Ws26elvMXIQk"
-};
--- rot/lib/cnf/goo.js
+++
@@ -1,5 +0,0 @@
-module.exports={
-id:"963533513410-99fm3hllj3isclhbhj9e38kr3gvd4tuj.apps.googleusercontent.com",
-    sec:"qhpJqhV1ugfxkPxxhqrXjP-W"
-}
-
